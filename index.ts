f6b087fb8610db320402fd70cccf69cdda475b15
Charles-D7625 <zguysanov.reon@gmail.com>
2023-12-28 20:31:08 +0300

    feat
    Реализовал получение токена и удаление токена с включением и отключением интеграции соответственно. Собственное api перенес в файл authSync

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 import express from "express";
 import { Request, Response } from "express";
-import  AmoCRM  from "./api/amo";
+import  AuthSync  from "./authSync";
 import { mainLogger } from "./logger"
 import config from "./config";
-import axios from "axios";
 import fs from "fs";
 
 const app = express();
@@ -11,90 +10,31 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const authCode = config.AUTH_CODE;
-const subDomain = config.SUB_DOMAIN;
-const api = new AmoCRM(subDomain, authCode);
-
-async function requestAccessTokenMy() {
-    return axios
-        .post(`https://${config.SUB_DOMAIN}.amocrm.ru/oauth2/access_token`, {
-            client_id: config.CLIENT_ID,
-            client_secret: config.CLIENT_SECRET,
-            grant_type: "authorization_code",
-            code: authCode,
-            redirect_uri: config.REDIRECT_URI, 
-        })
-        .then((res) => {
-            console.log(res.data);
-            return res.data; 
-        })
-        .catch((err) =>{
-            mainLogger.debug(err.response.data);
-            throw err;
-        });
+type RequestQuery = {
+    account_id: string,
+    code: string,
+    referer: string,
 }
 
-//недоделанная
-async function refreshTokenMy() {
-    return axios
-            .post(`https://${config.SUB_DOMAIN}.amocrm.ru/oauth2/access_token`, {
-                client_id: config.CLIENT_ID,
-                client_secret: config.CLIENT_SECRET,
-                grant_type: "refresh_token",
-                REFRESH_TOKEN: this.REFRESH_TOKEN,
-                redirect_uri: config.REDIRECT_URI,
-            })
-            .then((res) => {
-                this.logger.debug("Токен успешно обновлен");
-                const token = res.data;
-                fs.writeFileSync(this.AMO_TOKEN_PATH, JSON.stringify(token));
-                this.ACCESS_TOKEN = token.access_token;
-                this.REFRESH_TOKEN = token.refresh_token;
-                return token;
-            })
-            .catch((err) => {
-                this.logger.error("Не удалось обновить токен");
-                this.logger.error(err.response.data);
-            });
-}
+app.get("/login", async(req: Request<unknown, unknown, unknown, RequestQuery>, _res: Response) => {
 
-async function getAccountId(access_token: string) {
+    mainLogger.debug("LOGIN");
+     
+    const authCode: string = req.query.code;
+    const [subDomain] = req.query.referer.split('.');
 
-    const getAccountId = (() => {
-        return axios.get<any>(`https://${subDomain}.amocrm.ru/api/v4/account`, {
-            headers: {
-                Authorization: `Bearer ${access_token}`,
-            },
-        }).then((res) => res.data.id);
-    })
-    const accountId = await getAccountId();
+    const api = new AuthSync(subDomain, authCode);
 
-    return accountId;
-}
+    api.getAccessToken();
 
-//подумать как сделать правильно передачу id чтобы не обращаться постоянно к api и брать токен
-//Один раз нужно сохранить токен и к нему обращаться
-//Проверка с файлом не работает. Поищи как в if засунуть проверку
-app.get("/login", async (_req: Request, _res: Response) => {
-
-    const token = await requestAccessTokenMy();
-
-    const accountId = await getAccountId(token.access_token);
-
-    if(!fs.readFileSync(`./authclients/${accountId}_amo_token.json`)) {
-        mainLogger.debug('Token not found. Creating...');
-        fs.writeFileSync(`./authclients/${accountId}_amo_token.json`, JSON.stringify(token));
-    }
-    else {
-        mainLogger.debug('Token is already created');
-    }
 });
 
-app.get("/logout", async (_req: Request, _res: Response) => {
 
-    const token = await requestAccessTokenMy();
+app.get("/logout", async (req: Request<unknown, unknown, unknown, RequestQuery>, _res: Response) => {
 
-    const accountId = await getAccountId(token.access_token);
+    mainLogger.debug("LOGOUT");
+    
+    const accountId = req.query.account_id; 
 
     fs.unlink(`./authclients/${accountId}_amo_token.json`, err => {
         if(err) {
@@ -102,8 +42,8 @@ app.get("/logout", async (_req: Request, _res: Response) => {
         }
         mainLogger.debug('Token deleted successfully');
     });
+
 });
 
+
 app.listen(config.PORT,() => mainLogger.debug('Server started on ', config.PORT));
-
-
